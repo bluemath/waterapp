94bdbefc4a369f9a457ed2c00f65ff119022ec85
Ray Phillips <ray@bluem33.com>
2015-08-03 15:20:19 +0000

    switched to exec (wget) for cron instead of call

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends ConsoleKernel
 				foreach ($series as $s) {
 					$sitecode = $site->sitecode;
 					$variablecode = $s->variablecode;
-					//$url = url('/data/sites/' . $site->sitecode. '/' . $s->variablecode . '/update');
+					$url = url('/data/sites/' . $site->sitecode. '/' . $s->variablecode . '/update');
 					//$schedule->call('App\Http\Controllers\DataController@dataUpdate',compact('sitecode', 'variablecode'))->cron('*/5 * * * *');
 					$schedule->exec("wget -O/dev/null $url")->cron('*/5 * * * *');
 				}
